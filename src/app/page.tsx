31896b51600c496a1c3df1a2490645328212b8f0
Abdullah Ayman <abdallahayman834@gmail.com>
2026-09-01 21:08:17 +0300

    image loading fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
             </div>
             <Image
               src={"/img-0.png"}
-              loading="lazy"
               alt="personal photo"
               width={300}
               height={300}
